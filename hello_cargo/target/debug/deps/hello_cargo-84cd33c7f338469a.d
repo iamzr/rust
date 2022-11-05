eb1c7db4f29425ce44eda91e252f0712f4fd6353
Zia Rehman <iamziarehman@gmail.com>
2022-11-05 01:04:23 +0000

    Created hello world program

---
+++ hello_cargo/target/debug/deps/hello_cargo-84cd33c7f338469a.d
@@ -0,0 +1,5 @@
+/home/zia/TOP/rust/hello_cargo/target/debug/deps/hello_cargo-84cd33c7f338469a: src/main.rs
+
+/home/zia/TOP/rust/hello_cargo/target/debug/deps/hello_cargo-84cd33c7f338469a.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/zia/TOP/rust/hello_cargo/target/debug/hello_cargo: /home/zia/TOP/rust/hello_cargo/src/main.rs
